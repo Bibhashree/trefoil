f74aa418bc77114c3a6659513fab2ff6262295c8
Aayusha Sunuwar <aayu.snwr@gmail.com>
2025-07-25 13:57:27 +0545

    art and book pages, slider finalized

--- Trefoil.js
+++ Trefoil.js
@@ -1,34 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const images = [
-    "slider/hom1.jpg",
-    "slider/hom2.jpg",
-    "slider/hom3.jpg",
-    "slider/hom4.jpg",
-    "slider/hom5.jpg"
-  ];
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
 
-  let index = 0;
-  const sliderImage = document.querySelector(".slider-image");
-  const prevBtn = document.querySelector(".prev");
-  const nextBtn = document.querySelector(".next");
-
-  function showImage(i) {
-    sliderImage.src = images[i];
-  }
-
-  prevBtn.addEventListener("click", () => {
-    index = (index - 1 + images.length) % images.length;
-    showImage(index);
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
   });
-
-  nextBtn.addEventListener("click", () => {
-    index = (index + 1) % images.length;
-    showImage(index);
-  });
-
-  // Autoplay
-  setInterval(() => {
-    index = (index + 1) % images.length;
-    showImage(index);
-  }, 4000); // 4 seconds interval
-});
+}
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+setInterval(nextSlide, 4000);
